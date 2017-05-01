664619cedee162423079c74f7bde303da2a97374
Carlos Adrian Gomez <cagomez805@gmail.com>
2017-04-30 17:08:21 -0700

    Added Average Score and Time Elapsed plots

--- classify.py
+++ classify.py
@@ -1,31 +1,39 @@
 # Classifying handwritten digits using KNN, SVM, and MLP
-from sklearn import datasets, metrics, neighbors, svm
-from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import train_test_split
 from functools import wraps
 from time import time
 from collections import namedtuple
 
+from sklearn import datasets, metrics, neighbors, svm
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+# Our dataset
+digits = datasets.load_digits()
+
+# For tracking performance of each classifier
 PerformanceData = namedtuple("PerformanceData", ["score", "time_elapsed"])
 
 # A decorator used for timing functions
 def timed(f):
+
     @wraps(f)
     def wrapper(*args, **kwds):
         start = time()
         results = f(*args, **kwds)
         elapsed = time() - start
         return results, elapsed
+
     return wrapper
 
 def visualize_data():
-    import matplotlib.pyplot as plt
     images_and_labels = list(zip(digits.images, digits.target))
-    for index, (image, label) in enumerate(images_and_labels[:9]):
-        plt.subplot(2, 4, index + 1)
+
+    for index, (image, label) in enumerate(images_and_labels[:10]):
+        plt.subplot(2, 5, index + 1)
         plt.axis("off")
         plt.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-        plt.title("Training: {}".format(label))
+        plt.title("{}".format(label))
 
     plt.show()
 
@@ -34,6 +42,7 @@ def visualize_data():
 # http://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
 @timed
 def demo_knn(train_x, test_x, train_y, test_y, knn=None):
+
     if knn is None:
         knn = neighbors.KNeighborsClassifier(n_neighbors=3, n_jobs=-1)
 
@@ -49,7 +58,7 @@ def demo_knn(train_x, test_x, train_y, test_y, knn=None):
 # http://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html
 @timed
 def demo_svm(train_x, test_x, train_y, test_y, svm_classifier=None):
-    # Tweaking gamma provides different performance
+
     if svm_classifier is None:
         svm_classifier = svm.SVC(gamma=0.0001)
 
@@ -68,6 +77,7 @@ def demo_svm(train_x, test_x, train_y, test_y, svm_classifier=None):
 # http://scikit-learn.org/stable/modules/generated/sklearn.neural_network.MLPClassifier.html
 @timed
 def demo_mlp(train_x, test_x, train_y, test_y, mlp=None):
+
     if mlp is None:
         mlp = MLPClassifier(hidden_layer_sizes=(100, 100),
                             max_iter=400,
@@ -84,6 +94,7 @@ def demo_mlp(train_x, test_x, train_y, test_y, mlp=None):
     return metrics.accuracy_score(expected, predicted)
 
 def menu_selection():
+
     selection = 6
     try:
         selection = int(input("""
@@ -92,77 +103,162 @@ def menu_selection():
                 2) Demo Support vector machine (SVM)
                 3) Demo Multi-layer perceptron (MLP)
                 4) Demo All
-                5) Plot sample data (requires matplotlib)
+                5) Plot dataset sample
                 6) Quit\n
                 > """))
     except ValueError:
         pass
+
     print()
+
     return selection
 
 def num_iterations():
+
     iterations = 1
+
     try:
         iterations = int(input("""
                 Please enter the number of iterations:
                 > """))
     except ValueError:
         pass
+
     print()
+
     return iterations
 
 def run_classifier(iterations, func, dataset, test_size=0.10):
 
     performance_data = []
+
     for i in range(iterations):
         # Split data set into training and test sets
         train_x, test_x, train_y, test_y = train_test_split(dataset.data,
                                                             dataset.target,
                                                             test_size=test_size)
+
         accuracy, time_elapsed = func(train_x, test_x, train_y, test_y)
         performance_data.append(PerformanceData(accuracy, time_elapsed))
 
     return performance_data
 
-def compute_averages(performanceDataPoints, round_to=2):
-    avg_score = sum(dataPoint.score for dataPoint in performanceDataPoints) / len(performanceDataPoints)
-    avg_time_elapsed = sum(dataPoint.time_elapsed for dataPoint in performanceDataPoints) / len(performanceDataPoints)
-    return round(avg_score, round_to), round(avg_time_elapsed, round_to)
+def extract_performance_data(performance_data, metric):
+    metrics = {"score": 0, "time_elapsed": 1}
+    return [data[metrics[metric]] for data in performance_data]
 
-def run_tests(selection):
-    digits = datasets.load_digits()
-    iterations = num_iterations()
+def compute_averages(performanceDataPoints):
 
-    print("Training classifiers on {} handwritten digit samples for {} iterations."
-          .format(len(digits.data), iterations))
+    scores = extract_performance_data(performanceDataPoints, "score")
+    avg_score = sum(scores) / len(performanceDataPoints)
 
-    work_units = []
+    times = extract_performance_data(performanceDataPoints, "time_elapsed")
+    avg_time_elapsed = sum(times) / len(performanceDataPoints)
+
+    return avg_score, avg_time_elapsed
+
+def print_averages(results):
+
+    for classifier, performance in results.items():
+        avg_score = round(performance["avg_score"], 6)
+        avg_time_elapsed = round(performance["avg_time_elapsed"], 2)
+        print("{} score {}% in {}s".format(classifier,
+                                           avg_score,
+                                           avg_time_elapsed))
+
+def autolabel(ax, bars):
+
+    for bar in bars:
+        height = bar.get_height()
+        ax.text(bar.get_x() + bar.get_width() / 2., height,
+                round(height, 2), ha='center', va='bottom')
+
+
+def plot_average_time_elapsed(results):
+
+    classifiers = list(results.keys())
+    avg_time_elapsed = [results[c]["avg_time_elapsed"] for c in classifiers]
+
+    fig, ax = plt.subplots()
+    ax.set_title("Average Time Elapsed Learning the Digits Data set")
+    ax.set_ylabel("Average Time Elapsed (seconds)")
+
+    y_position = range(len(classifiers))
+    classifier_bars = ax.bar(y_position, avg_time_elapsed, align="center")
+
+    ax.set_xticks(y_position)
+    ax.set_xticklabels(classifiers)
+
+    autolabel(ax, classifier_bars)
+    plt.show(block=False)
+
+def plot_average_score(results):
+
+    classifiers = list(results.keys())
+    avg_score = [results[c]["avg_score"] for c in classifiers]
+
+    fig, ax = plt.subplots()
+    ax.set_title("Average Classifer Score on Digits Data set")
+    ax.set_ylabel("Average Score")
+
+    y_position = range(len(classifiers))
+    classifier_bars = ax.bar(y_position, avg_score, align="center")
+
+    ax.set_xticks(y_position)
+    ax.set_xticklabels(classifiers)
+
+    autolabel(ax, classifier_bars)
+    plt.show(block=False)
+
+def gather_demos(selection):
+
+    demos = []
+
     if selection == 1:
-        work_units.append(("KNN", demo_knn))
+        demos.append(("KNN", demo_knn))
     elif selection == 2:
-        work_units.append(("SVM", demo_svm))
+        demos.append(("SVM", demo_svm))
     elif selection == 3:
-        work_units.append(("MLP", demo_mlp))
+        demos.append(("MLP", demo_mlp))
     else:
-        print("Called")
-        work_units.append(("KNN", demo_knn))
-        work_units.append(("SVM", demo_svm))
-        work_units.append(("MLP", demo_mlp))
+        demos.append(("KNN", demo_knn))
+        demos.append(("SVM", demo_svm))
+        demos.append(("MLP", demo_mlp))
+
+    return demos
+
+def gather_results(demos, iterations):
 
     results = {}
-    for unit in work_units:
+
+    for unit in demos:
         classifier, fn = unit
         performance_data = run_classifier(iterations, fn, digits, test_size=0.10)
-        avg_accuracy, avg_time_elapsed = compute_averages(performance_data)
-        results[classifier] = (results.get(classifier, 0) + avg_accuracy,
-                                avg_time_elapsed)
+        avg_score, avg_time_elapsed = compute_averages(performance_data)
+        results[classifier] = {"avg_score": avg_score,
+                               "avg_time_elapsed": avg_time_elapsed,
+                               "performance_data": performance_data}
 
-    for classifier, averages in results.items():
-        avg_score, avg_time = round(averages[0], 2), round(averages[1], 2)
-        print("{} score {}% in {}s".format(classifier, avg_score, avg_time))
+    return results
+
+def run_tests(selection):
+
+    iterations = num_iterations()
+
+    print("Training classifiers on {} handwritten digit samples \
+          for {} iterations.\n".format(len(digits.data), iterations))
+
+    demos = gather_demos(selection)
+    results = gather_results(demos, iterations)
+
+    print_averages(results)
+    plot_average_score(results)
+    plot_average_time_elapsed(results)
 
 def main():
+
     try:
+
         selection = 0
         while selection != 6:
 
